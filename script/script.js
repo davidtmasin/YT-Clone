d5e1afccf54c432f35dbc58949bc4b33c4a308b7
David Teixeira de Masin <davidteixeira.info@gmail.com>
2021-07-28 11:29:01 -0300

    testando JS 2

--- script/script.js
+++ script/script.js
@@ -3,3 +3,13 @@ const inputSearch = document.querySelector('input[name="search"]')
 console.log(inputSearch)
 
 document.getElementById('YT-search').forEach(btn => btn.addEventListener('click', search))
+
+function search(event) {
+    event.preventDefault()
+  
+    const anchor = document.createElement('a')
+    anchor.target = "_blank"
+    anchor.href = `${event.target.getAttribute('formaction')}${encodeURI(inputSearch.value)}`
+    anchor.click()
+  }
+
